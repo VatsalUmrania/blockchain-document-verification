8e7a57a83051e42fe2029057796bb440b6b991e8
Shubham Maity <83867127+shub15@users.noreply.github.com>
2025-11-15 20:08:08 +0530

    add api base url to .env and some cleanup

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api" || 'http://localhost:5000/api';
 
 export const SIWE_STATEMENT = 'Sign in to Blockchain Document Verification';
 
